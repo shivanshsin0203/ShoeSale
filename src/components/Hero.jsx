ba9982080280d33c80e0a2f232dc362a50439f3c
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-10 15:36:22 +0530

    Created Hero Section Outline

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <div>
+            <h1></h1>
+            <h1></h1>
+            <button></button>
+            <div></div>
+            <div></div>
+            </div>
+            <div>
+                <img src='' alt='hero-img' className=''>
+                </img>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
