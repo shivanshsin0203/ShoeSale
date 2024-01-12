87e3a47c1a6c18a405c7b443bf1d6b7966dd8cc8
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-12 09:53:53 +0530

    Created Shoe section

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Item from './utils/Items.jsx'
+import Item from './utils/Item'
 import Title from './utils/Title' 
 
-const Sales = ({ endpoint: { title, items } }) => {
+const Sales = ({ ifExists, endpoint: { title, items } }) => {
   // console.log(endpoint)
   return (
    <>
       <div className='nike-container'>
         <Title title={title} />
-        <div className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 `}>
+        <div className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 ${ifExists ? 'grid-cols-3 xl:grid-cols-2 sm:grid-cols-1' : 'grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'}`}>
           {items?.map((item, i) => (
-            <Item {...item} key={i} />
+            <Item {...item} key={i} ifExists={ifExists} />
           ))}
         </div>
       </div>
