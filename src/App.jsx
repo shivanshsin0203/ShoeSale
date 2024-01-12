683afa9df1ed0c69576776f0715af4bdea1db33b
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-12 11:16:16 +0530

    Added Stories Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { Hero,Sales } from './components'
-import { heroapi,popularsales,toprateslaes } from './data/data'
-function App() {
+import { Hero,Sales,FlexContent,Stories } from './components'
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data.js';function App() {
   return (
     <main className='flex flex-col gap-16 relative'>
       <Hero heroapi={heroapi}/>
       <Sales endpoint={popularsales} ifExists/>
+      <FlexContent endpoint={highlight} ifExists />
       <Sales endpoint={toprateslaes}/>
+      <FlexContent endpoint={sneaker} />
+      <Stories story={story} />
     </main>
   )
 }
