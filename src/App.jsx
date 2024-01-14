67ea552c8fbe8d97d8457df9b8956d8ce1cd13c2
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-14 11:00:33 +0530

    Configured Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
-import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data.js';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
 const App = () => {
   return (
-   <>
-      <Navbar/>
-      <Cart />
-      <main className='flex flex-col gap-16 relative'>
-        <Hero heroapi={heroapi} />
-        <Sales endpoint={popularsales} ifExists />
-        <FlexContent endpoint={highlight} ifExists />
-        <Sales endpoint={toprateslaes} />
-        <FlexContent endpoint={sneaker} />
-        <Stories story={story} />
-      </main>
-      <Footer footerAPI={footerAPI} />
-   </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+  );
+};
 
 export default App;
