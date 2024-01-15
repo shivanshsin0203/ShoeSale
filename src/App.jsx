ed15a50f0729ee688923f670f220e415f979fa89
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-15 18:30:04 +0530

    Added products

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
+import Products from './Products.jsx';
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         </Routes>
     </Router>
   );
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
+import img1 from './assets/nike-adapt-bb-smart.png'
 const Products = () => {
+    const [img,setImg]=useState('');
+    async function getProducts(){
+        const response=await axios.get('http://localhost:3005/test');
+        console.log(response.data.data.items[2].img);
+      
+        setImg(response.data.data.items[11].img);
+    }
+
+    useEffect(()=>{
+        getProducts();
+    },[]);
+   
   return (
+    <>
     <div>Products</div>
+    <img src={`http://localhost:5173/src/${img}`} alt="image" />
+    </>
   )
 }
 
